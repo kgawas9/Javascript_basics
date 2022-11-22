1bd979de81a24b37410ade9e337ec7da93f2b4d7
Kiran <kgawas9@gmail.com>
2022-11-23 00:11:05 +0530

    Async code

--- async/async.js
+++ async/async.js
@@ -1,14 +1,29 @@
-const fetchData = (callback) => {
-    setTimeout(() => {
-        callback('Done');
-    }, 1500);
+// const fetchData = (callback) => {
+//     setTimeout(() => {
+//         callback('Done');
+//     }, 1500);
+// }
+
+// same function as above with Promise constructor
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done!');
+        }, 1500);
+    });
+    return promise;
 }
+
 
 setTimeout(() => {
     console.log('timer is done');
-    fetchData((text) => {
+    fetchData().then(text => {
         console.log(text);
+        return fetchData();
     })
+    .then(text2 => {
+        console.log(text2);
+    });
 }, 2000)
 
 console.log('Hello');
